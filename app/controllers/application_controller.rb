e124e7e527c82927dd8794081d8f65bb1fb6b157
marim0ss <piyomaru.bunbun@gmail.com>
2018-10-19 18:06:41 +0900

    deviseログイン時にuserのnicknameを登録(userテーブルにカラム追加)、 みんなの一覧：購入者はnickname登録していれば表示させる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # 以下を追記
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # nicknameをログイン時に使用する
+  # deviseに関するストロングパラメータを設定
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+  # ここまで追記
 end
